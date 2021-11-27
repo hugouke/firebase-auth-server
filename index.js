a39f97a2fff4707d7f1a2d63ce74b9328c3375ce
Hugo Nogueira <hugono.oliveira@gruposantander.com>
2021-11-27 13:12:08 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const express = require("express");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://studycase-60f0a.iam.gserviceaccount.com",
+});
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("static"));
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/login", function (req, res) {
+  res.render("login.html");
+});
+
+app.get("/signup", function (req, res) {
+  res.render("signup.html");
+});
+
+app.get("/profile", function (req, res) {
+  const sessionCookie = req.cookies.session || "";
+
+  admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true)
+    .then((userData) => {
+      console.log("Logged in:", userData.email);
+      res.render("profile.html");
+    })
+    .catch((error) => {
+      res.redirect("/login");
+    });
+});
+
+app.get("/", function (req, res) {
+  res.render("index.html");
+});
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  admin
+    .auth()
+    .createSessionCookie(idToken, { expiresIn })
+    .then(
+      (sessionCookie) => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.end(JSON.stringify({ status: "success" }));
+      },
+      (error) => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+      }
+    );
+});
+
+app.get("/sessionLogout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/login");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
